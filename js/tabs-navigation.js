859f80b987b50ee4f3eb2532b18a93f1fae7a1e3
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-12-30 02:50:45 +0200

    fix: :bug: fix tabs navigation

---
+++ js/tabs-navigation.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  //jquery for toggle sub menus
+  $('.sub-btn').click(function () {
+    $(this).next('.sub-menu').slideToggle();
+    $(this).find('.dropdown').toggleClass('rotate');
+  });
+});
+
+// add css classes when click on
+// sidebar
+const menuLinks = document.querySelectorAll('.home-wrapper__menu--item--link');
+// Function to set the 'active' class based on the current URL
+function setActiveClass() {
+  const currentUrl = window.location.href;
+
+  menuLinks.forEach((link) => {
+    const linkUrl = link.getAttribute('href');
+
+    if (currentUrl.includes(linkUrl) && linkUrl !== '#') {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+// Set initial 'active' class based on the current URL
+setActiveClass();
+
+// Add click event listener for navigation
+menuLinks.forEach((link) => {
+  link.addEventListener('click', function (event) {
+    // Check if the clicked link is not a logout link
+    if (!this.parentElement.classList.contains('logout')) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+
+      // Get the href attribute of the clicked link
+      const href = this.getAttribute('href');
+
+      // Manually navigate to the link's href
+      if (href && href !== '#') {
+        window.location.href = href;
+
+        // Set 'active' class after navigation
+        setActiveClass();
+      }
+    }
+  });
+});
+
+// categories
+const tabLinks = document.querySelectorAll('.home-wrapper__categories--item');
+tabLinks.forEach((link) => {
+  link.addEventListener('click', function () {
+    tabLinks.forEach((link) => {
+      link.classList.remove('active');
+    });
+    this.classList.add('active show');
+  });
+});
